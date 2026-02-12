8bee0ee8c5743cb987c2e68c1d0afef706b03179
nebobik1 <vanyapottter2000@gmail.com>
2026-02-12 20:50:34 +0300

    taskA done

--- src/by/it/group510901/halavachou/lesson01/FiboA.java
+++ src/by/it/group510901/halavachou/lesson01/FiboA.java
@@ -29,21 +29,30 @@ public class FiboA {
     }
 
     private int calc(int n) {
+
+        if (n <= 1)
+            return n;
+
+        return calc(n - 1) + calc(n - 2);
+
         //здесь простейший вариант, в котором код совпадает
         //с математическим определением чисел Фибоначчи
         //время O(2^n)
-        return 0;
+
     }
 
 
     BigInteger slowA(Integer n) {
-        //рекурсия
-        //здесь нужно реализовать вариант без ограничения на размер числа,
-        //в котором код совпадает с математическим определением чисел Фибоначчи
-        //время O(2^n)
+
+        if (n == 0) {
+            return BigInteger.ZERO;
+        }
+        if (n == 1) {
+            return BigInteger.ONE;
+        }
+        return slowA(n - 1).add(slowA(n - 2));
 
 
-        return BigInteger.ZERO;
     }
 
 
